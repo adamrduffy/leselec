0b8070a3298f9177d156093f8f79991c890123e7
adam.duffy <adamduffy@yahoo.com>
2017-11-13 15:58:18 +0000

    extracted common code to methods

--- src/main/groovy/org/adamrduffy/leselec/diagram/ArchDiagram.groovy
+++ src/main/groovy/org/adamrduffy/leselec/diagram/ArchDiagram.groovy
@@ -24,63 +24,65 @@ class ArchDiagram {
         }
         rows += 1
 
-        StringBuffer stringBuffer = new StringBuffer()
+        double radius = 0.4 / rows
+        List poslist = generateSeatPositionsInDiagram(rows, delegates, radius)
 
+        StringBuffer stringBuffer = new StringBuffer()
         stringBuffer << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
         stringBuffer << "<svg xmlns:svg=\"http://www.w3.org/2000/svg\"\n"
-        stringBuffer << "xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n"
-        stringBuffer << "width=\"360\" height=\"185\">\n"
+        stringBuffer << "     xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n"
+        stringBuffer << "     width=\"360\" height=\"185\">\n"
         stringBuffer << "<g>\n"
-        stringBuffer << "<text x=\"175\" y=\"175\" style=\"font-size:36px;font-weight:bold;text-align:center;text-anchor:middle;font-family:sans-serif\">$delegates</text>\n"
+        stringBuffer << "    <text x=\"175\" y=\"175\" style=\"font-size:36px;font-weight:bold;text-align:center;text-anchor:middle;font-family:sans-serif\">$delegates</text>\n"
 
-        def radius = 0.4 / rows
+        int totalCounter = 0
+        for (party in electedParties) {
+            def color = Party.getColor(party.code)
+            stringBuffer << generatePartySeatGroup(party.code, color, totalCounter, radius, party.totalSeats, poslist)
+            totalCounter += party.totalSeats
+        }
+        stringBuffer << generatePartySeatGroup("EMPTY", "#000000", totalCounter, radius, delegates - totalCounter, poslist)
+
+        stringBuffer << "</g>\n"
+        stringBuffer << "</svg>\n"
+
+        return stringBuffer.toString()
+    }
+
+    private static List generateSeatPositionsInDiagram(int rows, int delegates, double radius) {
         def poslist = []
         for (int i in 1..rows - 1) {
             int J = delegates / TOTALS[rows - 1] * Math.PI / (2 * Math.asin(2.0 / (3.0 * rows + 4.0 * i - 2.0))) as int
             double R = (3.0 * rows + 4.0 * i - 2.0) / (4.0 * rows)
-            if (J == 1) {
-                poslist << ([Math.PI / 2.0, 1.75 * R, R])
-            } else {
-                for (int j in 0..J-1) {
-                    double angle = j * (Math.PI - 2.0 * Math.sin(radius / R)) / (J - 1.0) + Math.sin(radius / R)
-                    poslist << ([angle, R * Math.cos(angle) + 1.75, R * Math.sin(angle)])
-                }
-            }
+            generateSeatPositionsInRow(J, poslist, R, radius)
         }
 
         // the last row with the left over seats
         int J = delegates - poslist.size()
         def R = (7.0 * rows - 2.0) / (4.0 * rows)
+        generateSeatPositionsInRow(J, poslist, R, radius)
+        // sort by position so that party seats are grouped together
+        return poslist.sort { a, b -> a[0] <=> b[0] }.reverse()
+    }
+
+    private static void generateSeatPositionsInRow(int J, List poslist, double R, double radius) {
         if (J == 1) {
             poslist << ([Math.PI / 2.0, 1.75 * R, R])
         } else {
-            for (int j in 0..J-1) {
+            for (int j in 0..J - 1) {
                 double angle = j * (Math.PI - 2.0 * Math.sin(radius / R)) / (J - 1.0) + Math.sin(radius / R)
                 poslist << ([angle, R * Math.cos(angle) + 1.75, R * Math.sin(angle)])
             }
         }
-        // sort by position so that party seats are grouped together
-        poslist = poslist.sort { a, b -> a[0] <=> b[0]}.reverse()
+    }
 
-        int totalCounter = 0
-        for (party in electedParties) {
-            def color = Party.getColor(party.code)
-            stringBuffer << "<g style=\"fill:$color\" id=\"$party.code\">\n"
-            for (int counter = totalCounter; counter < totalCounter + party.totalSeats; counter++) {
-                stringBuffer << sprintf("<circle cx=\"%.2f\" cy=\"%.2f\" r=\"%.2f\"/>\n", poslist[counter][1] * 100.0 + 5.0, 100.0 * (1.75 - (poslist[counter][2] as double)) + 5.0, radius * 100.0)
-            }
-            totalCounter += party.totalSeats
-            stringBuffer << "</g>\n"
+    static String generatePartySeatGroup(String code, String color, int totalCounter, double radius, int totalSeats, def poslist) {
+        StringBuffer stringBuffer = new StringBuffer()
+        stringBuffer << "    <g style=\"fill:$color\" id=\"$code\">\n"
+        for (int counter = totalCounter; counter < totalCounter + totalSeats; counter++) {
+            stringBuffer << sprintf("        <circle cx=\"%.2f\" cy=\"%.2f\" r=\"%.2f\"/>\n", poslist[counter][1] * 100.0 + 5.0, 100.0 * (1.75 - (poslist[counter][2] as double)) + 5.0, radius * 100.0)
         }
-        stringBuffer << "<g style=\"fill:#000000\" id=\"EMPTY\">\n"
-        for (int counter = totalCounter; counter < delegates; counter++) {
-            stringBuffer << sprintf("<circle cx=\"%.2f\" cy=\"%.2f\" r=\"%.2f\"/>\n", poslist[counter][1] * 100.0 + 5.0, 100.0 * (1.75 - (poslist[counter][2] as double)) + 5.0, radius * 100.0)
-        }
-        stringBuffer << "</g>\n"
-
-        stringBuffer << "</g>\n"
-        stringBuffer << "</svg>\n"
-
+        stringBuffer << "    </g>\n"
         return stringBuffer.toString()
     }
 
